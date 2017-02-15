11c20b60e9386b9144ce1efa4241baba722ccfdd
Eric Evans <eric@domainlanguage.com>
2017-02-15 18:17:47 -0500

    Missed file. Should have been in previous commit
    Something weird with the tool. These are the tests that go with the Allen's Interval Algebra logic.

--- test/string_time/step_n_time_tests.clj
+++ test/string_time/step_n_time_tests.clj
@@ -67,4 +67,36 @@
                  count)
              => 28))
 
+(facts "Examples of composing time transformations and stringifiers"
+       (fact (-> "2017-02-13" ((t-> identity))) => "2017-02-13")
+       (fact (-> "2017-02-13" ((t-> (enclosing :month)))) => "2017-02")
 
+       (fact "The composition can be a meaningful operation."
+             (let [last-day-of-month (t-> (enclosing :month) (nested-last :day))]
+               (last-day-of-month "2017-02-13") => "2017-02-28")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Allen's Interval Algebra  ;;;
+;;;  Basic relations           ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(facts "about Allen's Interval Algebra"
+       (fact "Thirteen basic relations in Allen's Interval Algebra"
+             (relation "2017" "2017") => :equal
+             (relation "2015" "2017") => :before
+             (relation "2017" "2015") => :after
+             (relation "2016" "2017") => :meets
+             (relation "2017" "2016") => :met-by
+             (relation "2017-01" "2017") => :starts
+             (relation "2017" "2017-01") => :started-by
+             (relation "2017-12" "2017") => :finishes
+             (relation "2017" "2017-12") => :finished-by
+             (relation "2017-02" "2017") => :during
+             (relation "2017" "2017-02") => :contains
+             (relation "2017-W05" "2017-02") => :overlaps
+             (relation "2017-02" "2017-W05") => :overlapped-by)
+
+       (fact "If scales are different."
+             (relation "2016-12" "2017") => :meets
+             (relation "2016-11" "2017") => :before
+             (relation "2017-W01" "2017") => :during))
