589d00892409dccf41ee2255965ac21285de803f
Eric Evans <eric@domainlanguage.com>
2017-12-25 00:33:39 -0500

    Joda Time rather than clj-time
    Make the project dependency Joda Time.
    When I started the project, JodaTime wasn't set up in a repo that I could just pull in, so the easiest workaround was to depend on clj-time (which had somehow made it work).

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time/clj-time "0.12.0"]
+                 [joda-time "2.9.9"]
                  [midje "1.8.3" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[midje "1.8.3" :exclusions [org.clojure/clojure]]]
                    :plugins      [[lein-midje "3.2"]]}}
